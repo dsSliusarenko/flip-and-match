6db187dd82070557b8d6a37e28bef44742485bf3
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-11-25 13:00:26 -0700

    created Angular material ang logo modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {LogoModule} from "./components/logo/logo.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        LogoModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
