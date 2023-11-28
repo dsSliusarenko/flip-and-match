b76ef7bd037095b122ad19326eb8f46d8e3b1207
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-11-27 20:30:52 -0700

    added timer

--- src/app/components/levels/easy/easy.component.ts
+++ src/app/components/levels/easy/easy.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Card} from "../card";
+import {Observable, Subject, takeUntil, timer} from 'rxjs';
 
 @Component({
   selector: 'fam-easy',
@@ -7,47 +8,54 @@ import {Card} from "../card";
   styleUrls: ['./easy.component.scss']
 })
 export class EasyComponent implements OnInit {
+  rxjsTimer: Observable<number> = timer(1000, 1000);
+  destroy: Subject<any> = new Subject();
+
+  timer: number = 0;
+
+
   demoCards: Card[] = [
     {
       icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
-      icon: 'test'
-    },{
+    }, {
       icon: 'test'
     }, {
       icon: 'test'
-    },{
+    }, {
       icon: 'test'
-    },{
+    }, {
       icon: 'test'
-    },{
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
+      icon: 'test'
+    }, {
       icon: 'test'
     }
   ]
 
   showCards!: Card[];
 
+
   ngOnInit() {
-
-
+    this.rxjsTimer.pipe(takeUntil(this.destroy)).subscribe(time => {
+      this.timer = time;
+    })
   }
-
 }
