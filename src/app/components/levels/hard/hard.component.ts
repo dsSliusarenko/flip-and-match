97696254c66a42d48b5f6ed32b10eee41a730219
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-11-27 19:29:34 -0700

    created button-container mixin and added levels component design

---
+++ src/app/components/levels/hard/hard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fam-hard',
+  templateUrl: './hard.component.html',
+  styleUrls: ['./hard.component.scss']
+})
+export class HardComponent {
+
+}
