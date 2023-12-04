8c42fc324317f4f50b5136fa96858646e3c305d0
Dmytro Sliusarenko <dmytro.sliusarenko@holdmycode.com>
2023-12-04 16:42:40 -0700

    created flip-cards-render module

---
+++ src/app/ui/flip-cards-render/flip-cards-render.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fam-flip-cards-render',
+  templateUrl: './flip-cards-render.component.html',
+  styleUrls: ['./flip-cards-render.component.scss']
+})
+export class FlipCardsRenderComponent {
+
+}
---
+++ src/app/ui/flip-cards-render/flip-cards-render.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlipCardsRenderComponent } from './flip-cards-render.component';
+
+
+
+@NgModule({
+  declarations: [
+    FlipCardsRenderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FlipCardsRenderModule { }
